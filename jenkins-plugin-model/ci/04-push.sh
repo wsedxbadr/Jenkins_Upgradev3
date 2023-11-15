c64be3c0f0b0b5e65cc36dc5963fbed843c221c7
wsedxbadr <148723235+wsedxbadr@users.noreply.github.com>
2023-11-15 18:47:04 +0100

    push2

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t wsedxbadr/jenkins-demo:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t wsedxbadr/jenkins-demo2:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
     echo 'Skipping login - credentials not set' 
@@ -7,4 +7,4 @@ else
     docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 fi
 
-docker push wsedxbadr/jenkins-demo:$1
+docker push wsedxbadr/jenkins-demo2:$1
