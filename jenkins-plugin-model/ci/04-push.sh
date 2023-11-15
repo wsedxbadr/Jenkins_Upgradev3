d369185cb4007e35d171ff35fe1f20fcc895e0b1
wsedxbadr <148723235+wsedxbadr@users.noreply.github.com>
2023-11-15 18:26:34 +0100

    Update 04-push.sh

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t wsedxbadr/jenkins-demo:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t wsedxbadr/jenkins-demo:$1 -f jenkins-plugin-model/src/demo-publish/dockerfile .
 
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
